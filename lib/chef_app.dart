920e645cd930f3c8a9b60caa290d70a7d14fbe9f
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-01-02 07:15:41 +0200

    create structure

---
+++ lib/chef_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChefApp extends StatelessWidget {
+  const ChefApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    ) ;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:chef_app/chef_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChefApp());
+}
+
