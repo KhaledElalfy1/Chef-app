f2fd225a680f47089788009fc3b6531403b5e392
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-02-20 07:32:58 +0200

    add screen utils

--- lib/chef_app.dart
+++ lib/chef_app.dart
@@ -1,16 +1,21 @@
 import 'package:chef_app/core/routes/app_routes.dart';
+import 'package:chef_app/core/themes/app_themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChefApp extends StatelessWidget {
   const ChefApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      initialRoute: Routes.initRoute,
-      onGenerateRoute: AppRouts.generateRoutes,
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+    return  ScreenUtilInit(
+      designSize:const Size(375, 812),
+      builder: (context, child) =>  MaterialApp(
+        initialRoute: Routes.initRoute,
+        onGenerateRoute: AppRouts.generateRoutes,
+        theme: themeData(),
+        debugShowCheckedModeBanner: false,
+      ),
     ) ;
   }
 }
