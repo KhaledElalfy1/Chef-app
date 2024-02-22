24ef59aca4aa1549dc18b8c4e477a5532a290459
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-02-22 06:32:22 +0200

    add extentions and bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:chef_app/chef_app.dart';
 import 'package:chef_app/core/database/cache/cache_helper.dart';
 import 'package:chef_app/core/global_controller/cubit/language_cubit.dart';
+import 'package:chef_app/core/helpers/bloc_observer.dart';
 import 'package:chef_app/core/services/server_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
+  Bloc.observer = MyBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
   setUp();
- await sl<CacheHelper>().init();
+  await sl<CacheHelper>().init();
   runApp(
     MultiBlocProvider(
       providers: [
